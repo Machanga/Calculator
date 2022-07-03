cf14afe0dafba4d87d00b36ee8590cf18fb31810
Florence Wanjiru <42543325+Machanga@users.noreply.github.com>
2022-07-03 07:46:33 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,24 @@
-let action = prompt('Please select the operation you want to perform: i.e +, -, *, / \n + for addition \n - for subtraction \n * for multiplication \n / for division');
-let first_number = prompt('Enter the first number: ');
-let second_number = prompt('Enter the second number: ');
+function calculator(){
+    let action = prompt('Please select the operation you want to perform: i.e +, -, *, / \n + for addition \n - for subtraction \n * for multiplication \n / for division');
+    let first_number = prompt('Enter the first number: ');
+    let second_number = prompt('Enter the second number: ');
 
-if (action == '+'){
-    sum = parseInt(first_number) + parseInt(second_number);
-    alert(first_number + '+ ' + second_number + '= ' + sum);
-} else if(action == '-'){
-    subtraction = parseInt(first_number) - parseInt(second_number);
-    alert(first_number + '- ' + second_number + '= ' + subtraction);
-} else if(action == '*'){
-    multiplication = parseInt(first_number) * parseInt(second_number);
-    alert(first_number + '* ' + second_number + '= ' + multiplication);
-} else if(action == '/'){
-    division = parseInt(first_number) / parseInt(second_number);
-    alert(first_number + '/ ' + second_number + '= ' + division);
-} else{
-    alert('Please select + or - or * or /. \n+ for addition \n - for subtraction \n * for multiplication \n / for division')
-    let action = prompt('Please select + or - or * or /')
-    let first_number = prompt('Enter the first number: ')
-    let second_number = prompt('Enter the second number: ')
+    if (action == '+'){
+        sum = parseInt(first_number) + parseInt(second_number);
+        alert(first_number + '+ ' + second_number + '= ' + sum);
+    } else if(action == '-'){
+        subtraction = parseInt(first_number) - parseInt(second_number);
+        alert(first_number + '- ' + second_number + '= ' + subtraction);
+    } else if(action == '*'){
+        multiplication = parseInt(first_number) * parseInt(second_number);
+        alert(first_number + '* ' + second_number + '= ' + multiplication);
+    } else if(action == '/'){
+        division = parseInt(first_number) / parseInt(second_number);
+        alert(first_number + '/ ' + second_number + '= ' + division);
+    } else{
+        alert('Please select + or - or * or /. \n+ for addition \n - for subtraction \n * for multiplication \n / for division');
+        calculator()
+    }
 }
+
+calculator()
